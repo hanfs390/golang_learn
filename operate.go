66a04f91150edc635b27043344f5f5c43ed89a1c
hanfs390 <32158191+hanfs390@users.noreply.github.com>
2020-03-14 20:16:50 +0800

    add linklist

---
+++ operate.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+/**
+1, group add
+2, group del
+3, group modify
+4, group addap
+5, group delap
+6, ap add
+7, ap del
+8, ap modify
+9, timer add
+10, timer del
+11, timer modify (connect to group)
+12, bw add
+13, bw del
+14, bw modify (connect to group)
+15, wlan add
+16, wlan del
+17, wlan modify (connect to group)
+ */
+
+
+func ConfHandle(msg json) {
+	/* 1 parse json */
+	/* 2 get handle */
+
+}
+
+
